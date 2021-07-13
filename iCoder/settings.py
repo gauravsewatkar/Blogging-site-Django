f3a3fd14392de23d4cc06cecb7be24560121418d
gaurav_sewatkar <53942720+gauravsewatkar@users.noreply.github.com>
2021-07-13 17:09:26 +0530

    k change

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i-0c5!h%jmns56bg%_t$xy9cc8ilz(7q$@tf!2!-dv2st627i_'
+SECRET_KEY = #add secret key here''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
